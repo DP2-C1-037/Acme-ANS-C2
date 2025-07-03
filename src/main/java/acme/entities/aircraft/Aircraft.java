966f7c707c4f968382eb388f18d024ef22743db1
AdrianChabrera <adrianchabrera@gmail.com>
2025-07-03 20:40:47 +0200

    added dashboard links from previous deliverables

--- src/main/java/acme/entities/aircraft/Aircraft.java
+++ src/main/java/acme/entities/aircraft/Aircraft.java
@@ -3,7 +3,9 @@ package acme.entities.aircraft;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -22,6 +24,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @ValidAircraft
+@Table(indexes = {
+	@Index(columnList = "registrationNumber")
+})
 public class Aircraft extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L; // olde Java remora
